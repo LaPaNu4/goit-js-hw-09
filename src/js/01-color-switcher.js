b596f28c0c74398e2325974b3d35f2323fc0d0d9
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-05-08 17:43:25 +0300

    1st complited

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  startbtn: document.querySelector('[data-start]'),
+  stopbtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.startbtn.addEventListener('click', startParty);
+
+refs.stopbtn.addEventListener('click', stopParty);
+
+let timerId = null;
+
+function startParty() {
+  refs.startbtn.disabled = true;
+  timerId = setInterval(() => {
+    const colorHex = getRandomHexColor();
+    console.log(colorHex);
+    refs.body.style.backgroundColor = colorHex;
+  }, 1000);
+}
+
+function stopParty() {
+  refs.startbtn.disabled = false;
+  clearInterval(timerId);
+  console.log(`timerStop`);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
